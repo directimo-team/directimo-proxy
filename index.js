18899ab7a9c4bf4fef4db6a8ce3d368cc2be5fea
Corneliu Dumitru <corneliu.dcv@gmail.com>
2023-12-25 17:41:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const app = express();
+const port = process.env.PORT || 4321;
+
+app.use("/resales", createProxyMiddleware({ target: "https://webapi.resales-online.com/V6", changeOrigin: true }));
+
+app.get("/ping", (req, res) => {
+	res.send("pong");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
